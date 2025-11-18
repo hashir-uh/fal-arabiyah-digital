b859ddc539b0a9943f0c800d24d450b3a21373a3
Muhammad Hashir <muhammadhashir23@gmail.com>
2025-11-18 17:08:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/fal-arabiyah-digital/",
   server: {
     host: "::",
     port: 8080,
